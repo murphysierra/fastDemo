f7d3df6c9199bb0c0c0a3c63a412ec458f002964
sierra_murphy <sierra_murphy@apple.com>
2021-08-04 16:59:11 -0700

    Add basic responsiveness to page;

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,13 @@
+import './MovieList.css';
+
 export function MovieList(props) {
 	const movies = props.movies;
 	const movieList = movies.map((result) => <MovieCard movie={result} />);
 
 	return (
-		<>
+		<div className="movie-list-container">
 			{movieList.length > 0 ? <ul>{movieList}</ul> : <></>}
-		</>
+		</div>
 	);
 }
 
