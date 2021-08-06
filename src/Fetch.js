90c98fa38923caf304e529576490041f528bcdfe
sierra_murphy <sierra_murphy@apple.com>
2021-08-05 19:57:28 -0700

    Change request params

--- src/Fetch.js
+++ src/Fetch.js
@@ -3,7 +3,16 @@ const apiKey = '4bfb31c1';
 export default function getMovieResults(searchInput, yearInput) {
 	const search = searchInput !== '' ? '&s=' + searchInput : '';
 	const year = yearInput !== '' ? '&y=' + yearInput : '';
-	return fetch(`http://www.omdbapi.com/?apikey=${apiKey}${search}${year}`)
+	return fetch(
+		`http://www.omdbapi.com/?apikey=${apiKey}${search}${year}`,
+		{
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+		)
 		.then(result => result.json())
 		.catch(error => console.warn('Error: ', error));
 }
