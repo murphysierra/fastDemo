6ea6c81fa192ebf2850c84218d16db52a181b919
sierra_murphy <sierra_murphy@apple.com>
2021-08-10 14:39:57 -0700

    Publishing tweaks

--- src/Fetch.js
+++ src/Fetch.js
@@ -3,16 +3,12 @@ const apiKey = '4bfb31c1';
 export default function getMovieResults(searchInput, yearInput) {
 	const search = searchInput !== '' ? '&s=' + searchInput : '';
 	const year = yearInput !== '' ? '&y=' + yearInput : '';
-	return fetch(
-		`http://www.omdbapi.com/?apikey=${apiKey}${search}${year}`,
-		{
-			method: 'GET',
+	return fetch( `http://www.omdbapi.com/?apikey=${apiKey}${search}${year}`, {
 			mode: 'cors',
 			headers: {
 				'Content-Type': 'application/json'
 			}
-		}
-		)
+		})
 		.then(result => result.json())
 		.catch(error => console.warn('Error: ', error));
 }
